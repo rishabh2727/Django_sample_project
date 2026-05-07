1abb0c202e0464cd9ba91a6c80ee46e1a8678b38
Rishabh Mehra <rishabhmehra2710@gmail.com>
2026-05-07 13:25:24 -0600

    basic structure of models ready. created migrations for all models.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+# We need a model for products, categories and tags, that will be stored in the
+# database and have fields acting as columns.
+# Product only belongs to one category, while a product can have multiple tags
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Products(models.Model):
+    id = models.AutoField(primary_key= True)
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits= 8, decimal_places= 2)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, related_name= 'products', on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag,related_name= 'products')
+
+
+    
+
+
+
+    
+
+
+
+
